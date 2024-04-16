77a809fd03a899a34642af546c0e09753e344ea9
Xamã <xcardoso1997@gmail.com>
2024-04-15 22:35:32 -0300

    questao 21 adicionada

--- q21.js
+++ q21.js
@@ -2,7 +2,6 @@ import {getnumber, write} from './utils/io_utils.js'
 
 
 function main(){
-    const n = getnumber('N: ')
     let soma = Number()
     for (let i = 1; i <= 50; i++){
         soma+= ((i*2)-1)/i

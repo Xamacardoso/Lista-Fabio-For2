8b5bb07db7dd0125f34070f39abdf23459993365
Xamã <xcardoso1997@gmail.com>
2024-04-15 22:07:06 -0300

    questao 17 adicionada

---
+++ q17.js
@@ -0,0 +1,13 @@
+import {getnumber, write} from './utils/io_utils.js'
+
+
+function main(){
+    const n = getnumber('N: ')
+    let soma = Number()
+    for (let i = 1; i <= n; i++){
+        soma+= 1/i
+    }
+    write(`Soma: ${soma.toFixed(2)}`)
+}
+
+main()
